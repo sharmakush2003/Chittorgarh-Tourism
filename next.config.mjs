3a6ebc62397b1fb2e40d5a473bcd5f29a0fb1f26
Kush Sharma <kushsharma.cor@gmail.com>
2026-03-11 22:29:46 +0530

    feat: enable offline mode PWA support via next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -12,4 +21,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
