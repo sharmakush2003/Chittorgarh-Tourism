6d768bdd760f4596a996277aac9852b928be6869
Kush Sharma <kushsharma.cor@gmail.com>
2026-03-19 22:06:04 +0530

    Fix: Correct CSS syntax in ExploreClient.js to restore page layout.

--- components/ExploreClient.js
+++ components/ExploreClient.js
@@ -176,8 +176,6 @@ export default function ExploreClient() {
             <style jsx>{`
                 /* ... other styles ... */
                 
-                .view-more-section {
-
                 .fixed-bg {
                     position: fixed;
                     inset: 0;
